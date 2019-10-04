17984a790873f209364cc1d6baead2e92cac8ffe
Davide Plessi <davideplessi@gmail.com>
2019-10-04 21:40:57 +0200

    Add and remove comment routes

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -156,4 +156,84 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 });
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post(
+    '/comment/:id',
+    [
+        auth,
+        [
+            check('text', 'Text is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id);
+            if (!user) {
+                return res.status(404).json({ msg: 'User not found!' });
+            }
+            const post = await Post.findById(req.params.id);
+            if (!post) {
+                return res.status(404).json({ msg: 'Post not found!' });
+            }
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: user.id
+            };
+
+            post.comments.unshift(newComment);
+
+            await post.save();
+
+            res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error!');
+        }
+    }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete post by id
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        const comment = post.comments.find(
+            comment => comment.id == req.params.comment_id
+        );
+
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment not found!' });
+        }
+
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        const removeIndex = post.likes
+            .map(c => c.user.toString())
+            .indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+        await post.save();
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(404).json({ msg: 'Not found!' });
+        }
+        res.status(500).send('Server error!');
+    }
+});
+
 module.exports = router;
