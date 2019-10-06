1fecf7ef2ef17860541388189ec204aa11527ffd
Davide Plessi <davideplessi@gmail.com>
2019-10-06 22:16:46 +0200

    Alert component & action call

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 //@ Layout
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 //@ Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -19,6 +20,7 @@ const App = () => (
                 <Navbar></Navbar>
                 <Route exact path='/' component={Landing}></Route>
                 <section className='container'>
+                    <Alert />
                     <Switch>
                         <Route exact path='/register' component={Register} />
                         <Route exact path='/login' component={Login} />
